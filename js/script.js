d1c93d4e3e04a67a0dab197f55b34b28aa33f86e
Liana <ltumikyan@gmail.com>
2017-03-31 13:59:36 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var btn = document.getElementById("draw");
+btn.addEventListener("click", function (event) {
+    event.preventDefault();
+    drawGrid();
+});
+function drawGrid() {
+    var mainDiv = document.getElementById("continer");
+    mainDiv.innerHTML = "";
+    var input = document.getElementById("inputNum");
+    var inpValue = input.value;
+    // if(isNaN(inpValue)){
+    //     alert("Please enter only number")
+    // }
+    for (var i = 0; i < inpValue; i++) {
+        var lineDiv = document.createElement("div");
+        mainDiv.appendChild(lineDiv);
+        lineDiv.className = "contineDiv";
+        for (var j = 0; j < inpValue; j++) {
+            lineDiv.innerHTML += "<div class='divInLine'></div>"
+
+        }
+    }
+
+}
+
