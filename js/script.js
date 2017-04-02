2e6b1a4d5cf20e38358e8daefe47f37c9bb7edf3
Liana <ltumikyan@gmail.com>
2017-04-02 12:19:03 -0700

    all functions replesed into IIFE function

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
-var btn = document.getElementById("draw");
-btn.addEventListener("click", function (event) {
-    event.preventDefault();
-    drawGrid();
-});
-function drawGrid() {
-    var mainDiv = document.getElementById("continer");
-    mainDiv.innerHTML = "";
-    var input = document.getElementById("inputNum");
-    var inpValue =+ input.value;
-    if(isNaN(inpValue)){
-        alert("Please enter only number")
+'use strict';
+(function IIFE (){
+
+    var board = {
+        btn:null,
+        mainDiv:null,
+        inputValue : null
+
+    };
+
+    function init () {
+
+        board.btn = document.getElementById("draw");
+        board.mainDiv= document.getElementById('continer');
+        board.inputValue = document.getElementById('inputNum');
+        bindEvents();
     }
-    for (var i = 0; i < inpValue; i++) {
-        var lineDiv = document.createElement("div");
-        mainDiv.appendChild(lineDiv);
-        lineDiv.className = "contineDiv";
-        for (var j = 0; j < inpValue; j++) {
-            lineDiv.innerHTML += "<div class='divInLine'></div>"
+
+
+    function bindEvents() {
+        board.btn.addEventListener('click', function (e) {
+            console.log('click');
+            e.preventDefault();
+            drawGrid();
+        })
+    }
+
+function drawGrid() {
+        board.mainDiv.innerHTML = "";
+        board.inputValue = parseInt(board.inputValue.value);
+        console.log(board.inputValue);
+        if (isNaN(board.inputValue)) {
+            alert('Please enter only number')
+        }
+
+    for (var i = 0; i < board.inputValue; i++) {
+        var lineDiv = document.createElement('div');
+        board.mainDiv.appendChild(lineDiv);
+        lineDiv.className = 'contineDiv';
+        for (var j = 0; j < board.inputValue; j++) {
+            lineDiv.innerHTML += '<div class="divInLine"></div>';
 
         }
     }
-
+init();
 }
+init();
+}  )();
 
