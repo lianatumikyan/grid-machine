8a793b88e2d11b0171b785b47507dadee29f14a5
Liana <ltumikyan@gmail.com>
2017-04-06 13:33:47 -0700

    coloring

--- js/script.js
+++ js/script.js
@@ -3,61 +3,102 @@
     var board = {
         form: null,
         primary: null,
-        inInput: null,
+        line : 0,
+        colBtn : null,
+        item : 0,
+        li : null
     };
-
 
     function init() {
         board.form = document.getElementById('infoForm');
         board.primary = document.getElementById('continer');
-        board.inInput = document.getElementById('inputNum');
+        board.li = document.getElementsByClassName('contineDiv');
+        board.colBtn = document.getElementById('coloring');
         bindEvents();
     }
 
     function bindEvents() {
         if (board.form) {
             if (board.form.addEventListener) {
-                board.form.addEventListener('submit', function (e) {
-                    drawGrid(e);
-                    e.preventDefault();
-                })
+                board.form.addEventListener('submit', drawGrid, false)
             } else if (board.form.attachEvent) {
                 board.form.attachEvent('onsubmit', drawGrid);
             }
-
         }
+       if (board.colBtn.addEventListener){
+            board.colBtn.addEventListener('click', colorSetInterval);
+        }else if (board.colBtn.attachEvent){
+           board.colBtn.attachEvent('onclick', colorSetInterval);
+       }
     }
 
-    function cleaner() {
+    function cleanContiner() {
         board.primary.innerHTML = '';
-        board.inInput.value = '';
+    }
+
+    function cleanInput() {
+        document.getElementById('inputNum').innerHTML = '';
     }
 
     function drawGrid(e) {
-        var parsedNum = parseInt(board.inInput.value);
-        if (isNaN(parsedNum)) {
+        clearInt();
+        e.preventDefault();
+        var number = parseInt(document.getElementById('inputNum').value);
+        if (isNaN(number)) {
             alert('Please enter only number');
             return;
         }
-        cleaner();
-        for (var i = 0; i < parsedNum; i++) {
+        cleanContiner();
+        for (var i = 0; i < number; i++) {
             var lineDiv = document.createElement('div');
             board.primary.appendChild(lineDiv);
             lineDiv.className = 'contineDiv';
-            for (var j = 0; j < parsedNum; j++) {
+            for (var j = 0; j < number; j++) {
                 lineDiv.innerHTML += '<div class="divInLine"></div>';
             }
         }
+
+        cleanInput();
+
     }
 
-    if (window.addEventListener) {
-        window.addEventListener('DOMContentLoaded',function (e) {
+    var interval;
+    function coloring() {
+        if(board.primary.innerHTML == ''){
+            alert('please first create gride before coloring it');
+            clearInterval(interval);
             return;
-        })
-    }else{
-        alert('Dom content is not loaded');
+        }
+        var red = Math.round(Math.random() * 255);
+        var green = Math.round(Math.random() * 255);
+        var blue = Math.round(Math.random() * 255);
+        var inLine = board.li[board.line].getElementsByClassName('divInLine');
+        inLine[board.item].style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue +')';
+        board.line++;
+        if (board.line == inLine.length) {
+            board.line = 0;
+            board.item++;
+
+        }
+        if (board.item == inLine.length) {
+            clearInt();
+        }
+    }
+    function colorSetInterval() {
+       clearInt();
+        interval = setInterval(coloring, 1000);
+    }
+    function clearInt() {
     }
 
-    init();
+
+
+    if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', init, false);
+    }
+    else {
+        document.attachEvent('onDOMContentLoaded', init);
+    }
+
 })();
 
