2e3dbfebec754e86a2e173ec271e81aed52036d7
Liana <ltumikyan@gmail.com>
2017-04-06 19:13:14 -0700

    added colorize function

--- js/script.js
+++ js/script.js
@@ -3,11 +3,19 @@
     var board = {
         form: null,
         primary: null,
+        line: 0,
+        item: 0,
+        colBtn: null,
+        lineContiner: null,
+        isRuning: false,
+        interval: null
     };
 
     function init() {
         board.form = document.getElementById('infoForm');
         board.primary = document.getElementById('continer');
+        board.lineContiner = document.getElementsByClassName('contineDiv');
+        board.colBtn = document.createElement('input');
         bindEvents();
     }
 
@@ -17,6 +25,14 @@
                 board.form.addEventListener('submit', drawGrid, false)
             } else if (board.form.attachEvent) {
                 board.form.attachEvent('onsubmit', drawGrid);
+            }
+        }
+        if (board.colBtn) {
+            console.log("liancho");
+            if (board.colBtn.addEventListener) {
+                board.colBtn.addEventListener('click', colorSetInterval);
+            } else if (board.colBtn.attachEvent) {
+                board.colBtn.attachEvent('onclick', colorSetInterval);
             }
         }
     }
@@ -30,11 +46,19 @@
     }
 
     function drawGrid(e) {
+        if (board.isRuning) {
+            clearInt();
+        }
         e.preventDefault();
         var number = parseInt(document.getElementById('inputNum').value);
         if (isNaN(number)) {
             alert('Please enter only number');
             return;
+        }
+        if (board.primary.innerHTML == '') {
+            board.form.appendChild(board.colBtn);
+            board.colBtn.value = 'Colorize';
+            board.colBtn.type = 'button';
         }
         cleanContiner();
         for (var i = 0; i < number; i++) {
@@ -46,6 +70,39 @@
             }
         }
         cleanInput();
+
+    }
+
+    function colorize() {
+        board.isRuning = true;
+        var red = Math.round(Math.random() * 255);
+        var green = Math.round(Math.random() * 255);
+        var blue = Math.round(Math.random() * 255);
+        var inLineContiner = board.lineContiner[board.line].getElementsByClassName('divInLine');
+        inLineContiner[board.item].style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+        board.line++;
+        if (board.line == inLineContiner.length) {
+            board.line = 0;
+            board.item++;
+
+        }
+        if (board.item == inLineContiner.length) {
+            clearInt();
+        }
+    }
+
+    function colorSetInterval() {
+        if (board.isRuning) {
+            return;
+        }
+        board.interval = setInterval(colorize, 1000);
+    }
+
+    function clearInt() {
+        board.item = 0;
+        board.line = 0;
+        board.isRuning = false;
+        clearInterval(board.interval);
     }
 
     if (document.addEventListener) {
